0534860f26b09c6db37bc1dae4efbcb592925fd1
Arpitkochar1 <143160100+Arpitkochar1@users.noreply.github.com>
2025-09-10 16:11:34 +0530

    Commit

---
+++ fooditem.js
@@ -0,0 +1,405 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Aloo Paratha',
+    category : 'biryani',
+    rating : 4.8,
+    price: 55,
+    img: 'https://www.cookwithkushi.com/wp-content/uploads/2021/03/IMG_7976_.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Egg Paratha',
+    category : 'biryani',
+    rating : 4.7,
+    price: 65,
+    img: 'https://static.toiimg.com/thumb/84547205.cms?imgsize=262619&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Paneer Paratha',
+    category : 'biryani',
+    rating : 4.9,
+    price: 70,
+    img: 'https://www.cookwithmanali.com/wp-content/uploads/2013/09/Paneer-Paratha-Homemade-500x500.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Chilli Potato',
+    category : 'biryani',
+    rating : 4.2,
+    price: 80,
+    img: 'https://static.toiimg.com/thumb/52532656.cms?width=1200&height=900',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Chole Bhature',
+    category : 'biryani',
+    rating : 4.6,
+    price: 75,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Chole_Bhature_from_Nagpur.JPG',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Chicken Roll',
+    category : 'biryani',
+    rating : 4.1,
+    price: 80,
+    img: 'https://uploads-ssl.webflow.com/5c481361c604e53624138c2f/60f2ea67b471327a1d82959b_chicken%20roll_1500%20x%201200.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Paneer Tikka Roll',
+    category : 'biryani',
+    rating : 4.0,
+    price: 80,
+    img: 'https://simmertoslimmer.com/wp-content/uploads/2021/06/Paneer-Kathi-Roll.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Veg Noodles',
+    category : 'biryani',
+    rating : 4.3,
+    price: 75,
+    img: 'https://www.cookwithmanali.com/wp-content/uploads/2014/11/Hakka-Noodles-1-500x375.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Chicken Noodles',
+    category : 'biryani',
+    rating : 4.5,
+    price: 85,
+    img: 'https://static.toiimg.com/thumb/54458787.cms?imgsize=153197&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Butter Chicken',
+    category : 'chicken',
+    rating : 4.7,
+    price: 341,
+    img: 'https://thumbs.dreamstime.com/b/butter-chicken-curry-27409035.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Rara Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 341,
+    img: 'https://madscookhouse.com/wp-content/uploads/2022/05/Chicken-Rara-Masala.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.1,
+    price: 341,
+    img: 'https://notoutofthebox.in/wp-content/uploads/2017/09/cd1.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Tikka Masala',
+    category : 'chicken',
+    rating : 4.8,
+    price: 341,
+    img: 'https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg?crop=0.683xw:1.00xh;0.221xw,0&resize=1200:*',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Kadhai Chicken',
+    category : 'chicken',
+    rating : 4.8,
+    price: 341,
+    img: 'https://static.toiimg.com/thumb/53523514.cms?width=1200&height=900',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Tandoori Roti',
+    category : 'chicken',
+    rating : 4.9,
+    price: 17,
+    img: 'https://www.cookwithmanali.com/wp-content/uploads/2021/07/Tandoori-Roti-500x500.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Veg Burger',
+    category : 'paneer',
+    rating : 4.4,
+    price: 50,
+    img: 'https://www.vegrecipesofindia.com/wp-content/uploads/2020/12/burger-recipe-1.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Crispy Patty Burger',
+    category : 'paneer',
+    rating : 4.8,
+    price: 60,
+    img: 'https://i.ytimg.com/vi/6DTLyEsgmyk/maxresdefault.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Maharaja Burger',
+    category : 'paneer',
+    rating : 4.9,
+    price: 100,
+    img: 'https://mcdonaldsblog.in/wp-content/uploads/2016/11/maharaja-mac-story_McDonalds_081116.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Hara Bhara Burger',
+    category : 'paneer',
+    rating : 4.3,
+    price: 70,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/03/hara-bhara-burger.jpg.webp',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Cheese Fries',
+    category : 'paneer',
+    rating : 4.8,
+    price: 60,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Shake_shack_cheese_fries.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Peri Peri Fries',
+    category : 'paneer',
+    rating : 4.8,
+    price: 70,
+    img: 'https://cookingwithparita.com/wp-content/uploads/2022/10/image-of-baked-crispy-peri-peri-fries-recipe-2.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Oreo Milkshake',
+    age: '32',
+    category : 'paneer',
+    rating : 4.5,
+    price: 75,
+    img: 'https://www.whiskaffair.com/wp-content/uploads/2020/07/Oreo-Milkshake-2-1-1200x1800.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Grilled Sandwich',
+    category : 'vegetable',
+    rating : 4.1,
+    price: 60,
+    img: 'https://static.toiimg.com/thumb/54714340.cms?imgsize=458099&width=800&height=800',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Paneer Tikka Sandwich',
+    category : 'vegetable',
+    rating : 4.6,
+    price: 70,
+    img: 'https://i0.wp.com/kalimirchbysmita.com/wp-content/uploads/2018/10/Paneer-Tikka-Sandwich-Insta-02.jpg?resize=1537%2C1024',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Chicken Sandwich',
+    category : 'vegetable',
+    rating : 4.1,
+    price: 75,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2016/07/chicken-sandwich-with-mayo-500x375.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Red Sauce Pasta',
+    category : 'vegetable',
+    rating : 4.4,
+    price: 75,
+    img: 'https://c.ndtvimg.com/2020-01/0ta23aq8_pasta_625x300_24_January_20.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'White Sauce Pasta',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 80,
+    img: 'https://www.whiskaffair.com/wp-content/uploads/2021/05/White-Sauce-Paste-2-3.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Panner Patty',
+    category : 'vegetable',
+    rating : 4.9,
+    price: 40,
+    img: 'https://cdn.shopify.com/s/files/1/0279/6329/3831/products/PANEER_PUFF_1024x1024.jpg?v=1580715207',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Chicken Patty',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 45,
+    img: 'https://justbakedcake.com/wp-content/uploads/2020/11/Chicken-patty.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Plain Maggi',
+    category : 'chinese',
+    rating : 4.5,
+    price: 40,
+    img: 'https://lh3.googleusercontent.com/EcPO-fd6siyin4WVZXINk1yYcmMWOZhYySSHNDGHwpKTjtzOCmDTAtHlbDpQkLi83eZ1KNZ4H2jgyPBBUPoyGAfkTIglRQq0WW48pUAF',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Masala Maggi',
+    category : 'chinese',
+    rating : 4.5,
+    price: 45,
+    img: 'https://spoonsofflavor.com/wp-content/uploads/2019/09/Simple-Maggi-Masala-Noodles-At-Home.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Peri Peri Maggi',
+    category : 'chinese',
+    rating : 4.5,
+    price: 50,
+    img: 'https://www.whiskaffair.com/wp-content/uploads/2021/05/Peri-Peri-Maggi-2-3.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Butter Maggi',
+    category : 'chinese',
+    rating : 4.3,
+    price: 55,
+    img: 'https://lh3.googleusercontent.com/JWqOHKiCiia2jT6fjuJrcWCoYWJYmxP7k18JN6CtovTknxhHPd0LG9VRYCZmxgBmpCTnWR0DA0yZDI_welstXMvgisfirNu7Sep2K70',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Butter Garlic Maggi',
+    category : 'chinese',
+    rating : 4.3,
+    price: 55,
+    img: 'https://i.ytimg.com/vi/6uVZtcBJCiQ/maxresdefault.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Choco Lava Cake',
+    category : 'chinese',
+    rating : 4.8,
+    price: 70,
+    img: 'https://www.bakingo.com/blog/wp-content/uploads/2022/06/best-choco-lava-cake-recipe-ever-1200x900.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Choco Truffle',
+    category : 'chinese',
+    rating : 4.9,
+    price: 65,
+    img: 'https://static.toiimg.com/photo/75758092.cms',
+    quantity: 1
+},
+{
+    id: 37,
+    name: ' Masala Dosa',
+    category : 'south indian',
+    rating : 4.9,
+    price: 80,
+    img: 'https://vismaifood.com/storage/app/uploads/public/8b4/19e/427/thumb__1200_0_0_0_auto.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli Sambhar',
+    category : 'south indian',
+    rating : 4.5,
+    price: 70,
+    img: 'https://www.nehascookbook.com/wp-content/uploads/2022/09/Instant-idli-sambar-WS-1.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: ' Rava Masala Dosa',
+    category : 'south indian',
+    rating : 4.5,
+    price: 100,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2022/05/rava-dosa-recipe.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Malabar Parotta',
+    category : 'south indian',
+    rating : 4.7,
+    price: 90,
+    img: 'https://www.whiskaffair.com/wp-content/uploads/2020/04/Kerala-Parotta-3.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Uttapam',
+    category : 'south indian',
+    rating : 4.5,
+    price: 80,
+    img: 'https://rakskitchen.net/wp-content/uploads/2013/03/8527219504_0ddb2cde6f_z-500x500.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Paneer Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 90,
+    img: 'https://i.ytimg.com/vi/Vb1j8bd62kI/maxresdefault.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Upma',
+    category : 'south indian',
+    rating : 4.9,
+    price: 90,
+    img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/upma-recipe.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Vada Sambhar',
+    category : 'south indian',
+    rating : 4.7,
+    price: 100,
+    img: 'https://img.freepik.com/premium-photo/sambar-vada-medu-vada-popular-south-indian-food-served-with-green-red-coconut-chutney-moody-background-selective-focus_466689-59626.jpg?w=2000',
+    quantity: 1
+},
+]
+
+export {foodItem};
+
